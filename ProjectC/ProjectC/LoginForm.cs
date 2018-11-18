a35665759f06d8fdd38cca745e43ac177d5dc42e
PtTB0610 <34046214+PtTB0610@users.noreply.github.com>
2018-11-18 19:40:19 +0700

    Login form & SQLScript
    thử login bằng stored procedure và chỉnh sửa lại file sql

--- ProjectC/ProjectC/LoginForm.cs
+++ ProjectC/ProjectC/LoginForm.cs
@@ -25,20 +25,39 @@ namespace ProjectC
             //SqlDataAdapter sda = new SqlDataAdapter("SELECT usertype FROM WHERE username = '" + txtUsername.Text + "' and password = '" + txtPassword.Text + "'", con);
             //DataTable dt = new DataTable();
             //sda.Fill(dt);
-            SqlCommand cmd = new SqlCommand("SELECT * FROM [USER] WHERE USER_NAME = '" + txtUsername.Text + "' and USER_PASS = '" + txtPassword.Text + "' AND USER_STATUS = 'active'", con);
-            con.Open();
-            SqlDataReader sdr = cmd.ExecuteReader();
-            if (sdr.Read() == true)
+            //SqlCommand cmd = new SqlCommand("SELECT * FROM [USER] WHERE USER_NAME = '" + txtUsername.Text + "' and USER_PASS = '" + txtPassword.Text + "' AND USER_STATUS = 'active'", con);
+            //SqlCommand cmd = new SqlCommand("SP_LAYDSUSER", con);
+            //cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sda = new SqlDataAdapter("SP_LAYDSUSER",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            for (int i = 0; i < dt.Rows.Count; i++)
             {
-                this.Close();
-                userType = (string)sdr["USER_TYPE"];
-                flag = true;
-                clsFormProvider.mainF.Show();
-                //this.Hide();
-                //MainForm mF = new MainForm((string)sdr["user_type"]);
-                //mF.Show(this);
+                DataRow dr = dt.Rows[i];
+                if (dr["USER_NAME"].ToString() == txtUsername.Text && dr["USER_PASS"].ToString() == txtPassword.Text)
+                {
+                    this.Close();
+                    userType = (string)dr["USER_TYPE"];
+                    flag = true;
+                    clsFormProvider.mainF.Show();
+                }
             }
-            else MessageBox.Show("Wrong username or password!!! Please reenter ", "Login error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            if (!flag) {
+                MessageBox.Show("Wrong username or password!!! Please reenter ", "Login error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //con.Open();
+            //SqlDataReader sdr = cmd.ExecuteReader();
+            //if (sdr.Read() == true)
+            //{
+            //    this.Close();
+            //    userType = (string)sdr["USER_TYPE"];
+            //    flag = true;
+            //    clsFormProvider.mainF.Show();
+            //    //this.Hide();
+            //    //MainForm mF = new MainForm((string)sdr["user_type"]);
+            //    //mF.Show(this);
+            //}
+            //else MessageBox.Show("Wrong username or password!!! Please reenter ", "Login error", MessageBoxButtons.OK,MessageBoxIcon.Error);
         }
 
         public string getUserType() {
